17c037dc99a5c1648e5b08d51e3e539513d40100
muyu <1744391048@qq.com>
2026-03-17 20:45:07 +0800

    新增了point类，方便画面操作

---
+++ math/Point.h
@@ -0,0 +1,20 @@
+//
+// Created by muyuchen on 2026/3/17.
+//
+
+#ifndef INC_3DENGINE_POINT_H
+#define INC_3DENGINE_POINT_H
+
+class Point
+{
+public:
+    int x;
+    int y;
+
+    Point();
+
+    Point(int x,int y);
+
+};
+
+#endif //INC_3DENGINE_POINT_H
